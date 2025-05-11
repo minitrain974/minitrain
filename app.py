be2fda89cb8b254cc17f7d2db932597e580da31a
minitrain974 <minit7876@gmail.com>
2025-05-11 15:37:56 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request, jsonify
 import networkx as nx
 
@@ -78,4 +79,5 @@ def calculate_minimum_spanning_tree(edges):
     return mst
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(debug=True, host='0.0.0.0', port=port)
